46da04858db29ea579858ad18070fea10047800a
Patrick Swaney <prswaney@gmail.com>
2020-08-10 13:33:18 -0700

    fix errors with music and date of birth.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-$(document).ready(fuction() {
+$(document).ready(function() {
   $("#formOne").submit(function(event) {
     const nameInput = $("input#name").val();
-    const dateInput = $("#date").val();
+    const dateInput = $("#born").val();
     const favoriteFood = $("#favoriteFood").val();
-    const favoriteMusic = $("input:radio[favoriteMusic]:checked").val();
+    const favoriteMusic = $("input:radio[name=favoriteMusic]:checked").val();
+    console.log(favoriteMusic)
     const favoriteColor = $("#favoriteColor").val();
 
     $(".name").text(nameInput);
-    $(".date").text(dateInput);
+    $(".born").text(dateInput);
     $(".favoriteFood").text(favoriteFood);
     $(".favoriteMusic").text(favoriteMusic);
     $(".favoriteColor").text(favoriteColor);
 
     $("#survey").show();
+
+    $("#name").val('');
+    $("#born").val('');
+    $("#favoriteFood").val('');
+    $("#favoriteMusic").val('');
+    $("#favoriteColor").val('');
 
     event.preventDefault();
   })
